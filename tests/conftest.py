lymanepp/ha-smart-controller

rename to smartify

tests/conftest.py
@@ -26,7 +26,6 @@ def auto_enable_custom_integrations(
 @pytest.fixture(autouse=True)
 def suppress_entity_sw_version_patch(monkeypatch):
     monkeypatch.setattr(
-        "custom_components.smart_controller.entity."
-        "SmartControllerEntity._set_sw_version",
+        "custom_components.smartify.entity.SmartifyControllerEntity._set_sw_version",
         AsyncMock(),
     )

tests/test_exhaust_fan_controller.py
@@ -5,8 +5,8 @@ from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.smart_controller.const import Config
-from custom_components.smart_controller.exhaust_fan_controller import (
+from custom_components.smartify.const import Config
+from custom_components.smartify.exhaust_fan_controller import (
     ExhaustFanController,
 )
 
@@ -41,7 +41,7 @@ async def test_high_humidity_turns_fan_on(
     )
 
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify_controller",
         data={
             Config.CONTROLLED_ENTITY: "fan.bathroom",
             Config.TEMP_SENSOR: "sensor.temp",
@@ -62,9 +62,7 @@ async def test_high_humidity_turns_fan_on(
 
     await controller.async_setup(hass)
 
-    await controller.on_state_change(
-        hass.states.get("sensor.humidity")
-    )
+    await controller.on_state_change(hass.states.get("sensor.humidity"))
 
     controller.async_service_call.assert_called()
 
@@ -101,7 +99,7 @@ async def test_low_humidity_turns_fan_off(
     )
 
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify_controller",
         data={
             Config.CONTROLLED_ENTITY: "fan.bathroom",
             Config.TEMP_SENSOR: "sensor.temp",
@@ -122,9 +120,7 @@ async def test_low_humidity_turns_fan_off(
 
     await controller.async_setup(hass)
 
-    await controller.on_state_change(
-        hass.states.get("sensor.humidity")
-    )
+    await controller.on_state_change(hass.states.get("sensor.humidity"))
 
     controller.async_service_call.assert_called()
 
@@ -156,7 +152,7 @@ async def test_invalid_humidity_does_not_crash(
     )
 
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify_controller",
         data={
             Config.CONTROLLED_ENTITY: "fan.bathroom",
             Config.TEMP_SENSOR: "sensor.temp",
@@ -177,9 +173,7 @@ async def test_invalid_humidity_does_not_crash(
 
     await controller.async_setup(hass)
 
-    await controller.on_state_change(
-        hass.states.get("sensor.humidity")
-    )
+    await controller.on_state_change(hass.states.get("sensor.humidity"))
 
     controller.async_service_call.assert_not_called()
 

tests/test_init.py
@@ -10,7 +10,7 @@ async def test_async_setup_entry(
     hass: HomeAssistant,
 ):
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify_controller",
         data={},
     )
 
@@ -20,9 +20,7 @@ async def test_async_setup_entry(
     controller.async_setup = AsyncMock()
     controller.async_unload = MagicMock()
 
-    with patch(
-        "custom_components.smart_controller._create_controller"
-    ) as mock_create:
+    with patch("custom_components.smartify._create_controller") as mock_create:
         mock_create.return_value = controller
 
         result = await hass.config_entries.async_setup(
@@ -39,7 +37,7 @@ async def test_async_unload_entry(
     hass: HomeAssistant,
 ):
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify_controller",
         data={},
     )
 
@@ -49,9 +47,7 @@ async def test_async_unload_entry(
     controller.async_setup = AsyncMock()
     controller.async_unload = MagicMock()
 
-    with patch(
-        "custom_components.smart_controller._create_controller"
-    ) as mock_create:
+    with patch("custom_components.smartify._create_controller") as mock_create:
         mock_create.return_value = controller
 
         result = await hass.config_entries.async_setup(

tests/test_reload_regression.py
@@ -10,7 +10,7 @@ async def test_duplicate_reload_replaces_old_controller(
     hass: HomeAssistant,
 ):
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify_controller",
         data={},
     )
 
@@ -24,9 +24,7 @@ async def test_duplicate_reload_replaces_old_controller(
     new_controller.async_setup = AsyncMock()
     new_controller.async_unload = MagicMock()
 
-    with patch(
-        "custom_components.smart_controller._create_controller"
-    ) as mock_create:
+    with patch("custom_components.smartify._create_controller") as mock_create:
         mock_create.side_effect = [
             old_controller,
             new_controller,

tests/test_smartify_controller_context.py
@@ -1,12 +1,11 @@
-
 import pytest
 from homeassistant.core import Context, Event, HomeAssistant, State
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.smart_controller.smart_controller import SmartController
+from custom_components.smartify.smartify_controller import SmartifyController
 
 
-class DummyController(SmartController):
+class DummyController(SmartifyController):
     async def on_state_change(self, state):
         self.state_calls += 1
 
@@ -24,7 +23,7 @@ async def test_self_generated_context_ignored(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify_controller",
         ),
         "off",
     )
@@ -33,9 +32,7 @@ async def test_self_generated_context_ignored(
 
     context = Context()
 
-    controller._service_context_ids.add(
-        context.id
-    )
+    controller._service_context_ids.add(context.id)
 
     event = Event(
         "state_changed",

tests/test_smartify_controller_timers.py
@@ -4,10 +4,10 @@ import pytest
 from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.smart_controller.smart_controller import SmartController
+from custom_components.smartify.smartify_controller import SmartifyController
 
 
-class DummyController(SmartController):
+class DummyController(SmartifyController):
     async def on_state_change(self, state):
         pass
 
@@ -25,7 +25,7 @@ async def test_set_timer_creates_timer(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify_controller",
         ),
         "off",
     )
@@ -46,7 +46,7 @@ async def test_set_timer_replaces_existing_timer(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify_controller",
         ),
         "off",
     )
@@ -74,7 +74,7 @@ async def test_set_timer_none_cancels_timer(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify_controller",
         ),
         "off",
     )
@@ -97,7 +97,7 @@ async def test_async_unload_cleans_up_timer(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify_controller",
         ),
         "off",
     )

lymanepp/ha-smartify

tests/conftest.py
@@ -26,7 +26,6 @@ def auto_enable_custom_integrations(
 @pytest.fixture(autouse=True)
 def suppress_entity_sw_version_patch(monkeypatch):
     monkeypatch.setattr(
-        "custom_components.smart_controller.entity."
-        "SmartControllerEntity._set_sw_version",
+        "custom_components.smartify.entity.SmartifyEntity._set_sw_version",
         AsyncMock(),
     )

tests/test_exhaust_fan_controller.py
@@ -5,8 +5,8 @@ from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.smart_controller.const import Config
-from custom_components.smart_controller.exhaust_fan_controller import (
+from custom_components.smartify.const import Config
+from custom_components.smartify.exhaust_fan_controller import (
     ExhaustFanController,
 )
 
@@ -41,7 +41,7 @@ async def test_high_humidity_turns_fan_on(
     )
 
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify",
         data={
             Config.CONTROLLED_ENTITY: "fan.bathroom",
             Config.TEMP_SENSOR: "sensor.temp",
@@ -62,9 +62,7 @@ async def test_high_humidity_turns_fan_on(
 
     await controller.async_setup(hass)
 
-    await controller.on_state_change(
-        hass.states.get("sensor.humidity")
-    )
+    await controller.on_state_change(hass.states.get("sensor.humidity"))
 
     controller.async_service_call.assert_called()
 
@@ -101,7 +99,7 @@ async def test_low_humidity_turns_fan_off(
     )
 
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify",
         data={
             Config.CONTROLLED_ENTITY: "fan.bathroom",
             Config.TEMP_SENSOR: "sensor.temp",
@@ -122,9 +120,7 @@ async def test_low_humidity_turns_fan_off(
 
     await controller.async_setup(hass)
 
-    await controller.on_state_change(
-        hass.states.get("sensor.humidity")
-    )
+    await controller.on_state_change(hass.states.get("sensor.humidity"))
 
     controller.async_service_call.assert_called()
 
@@ -156,7 +152,7 @@ async def test_invalid_humidity_does_not_crash(
     )
 
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify",
         data={
             Config.CONTROLLED_ENTITY: "fan.bathroom",
             Config.TEMP_SENSOR: "sensor.temp",
@@ -177,9 +173,7 @@ async def test_invalid_humidity_does_not_crash(
 
     await controller.async_setup(hass)
 
-    await controller.on_state_change(
-        hass.states.get("sensor.humidity")
-    )
+    await controller.on_state_change(hass.states.get("sensor.humidity"))
 
     controller.async_service_call.assert_not_called()
 

tests/test_init.py
@@ -10,7 +10,7 @@ async def test_async_setup_entry(
     hass: HomeAssistant,
 ):
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify",
         data={},
     )
 
@@ -20,9 +20,7 @@ async def test_async_setup_entry(
     controller.async_setup = AsyncMock()
     controller.async_unload = MagicMock()
 
-    with patch(
-        "custom_components.smart_controller._create_controller"
-    ) as mock_create:
+    with patch("custom_components.smartify._create_controller") as mock_create:
         mock_create.return_value = controller
 
         result = await hass.config_entries.async_setup(
@@ -39,7 +37,7 @@ async def test_async_unload_entry(
     hass: HomeAssistant,
 ):
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify",
         data={},
     )
 
@@ -49,9 +47,7 @@ async def test_async_unload_entry(
     controller.async_setup = AsyncMock()
     controller.async_unload = MagicMock()
 
-    with patch(
-        "custom_components.smart_controller._create_controller"
-    ) as mock_create:
+    with patch("custom_components.smartify._create_controller") as mock_create:
         mock_create.return_value = controller
 
         result = await hass.config_entries.async_setup(

tests/test_reload_regression.py
@@ -10,7 +10,7 @@ async def test_duplicate_reload_replaces_old_controller(
     hass: HomeAssistant,
 ):
     entry = MockConfigEntry(
-        domain="smart_controller",
+        domain="smartify",
         data={},
     )
 
@@ -24,9 +24,7 @@ async def test_duplicate_reload_replaces_old_controller(
     new_controller.async_setup = AsyncMock()
     new_controller.async_unload = MagicMock()
 
-    with patch(
-        "custom_components.smart_controller._create_controller"
-    ) as mock_create:
+    with patch("custom_components.smartify._create_controller") as mock_create:
         mock_create.side_effect = [
             old_controller,
             new_controller,

tests/test_smartify_controller_context.py
@@ -1,12 +1,11 @@
-
 import pytest
 from homeassistant.core import Context, Event, HomeAssistant, State
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.smart_controller.smart_controller import SmartController
+from custom_components.smartify.smartify_controller import SmartifyController
 
 
-class DummyController(SmartController):
+class DummyController(SmartifyController):
     async def on_state_change(self, state):
         self.state_calls += 1
 
@@ -24,7 +23,7 @@ async def test_self_generated_context_ignored(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify",
         ),
         "off",
     )
@@ -33,9 +32,7 @@ async def test_self_generated_context_ignored(
 
     context = Context()
 
-    controller._service_context_ids.add(
-        context.id
-    )
+    controller._service_context_ids.add(context.id)
 
     event = Event(
         "state_changed",

tests/test_smartify_controller_timers.py
@@ -4,10 +4,10 @@ import pytest
 from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.smart_controller.smart_controller import SmartController
+from custom_components.smartify.smartify_controller import SmartifyController
 
 
-class DummyController(SmartController):
+class DummyController(SmartifyController):
     async def on_state_change(self, state):
         pass
 
@@ -25,7 +25,7 @@ async def test_set_timer_creates_timer(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify",
         ),
         "off",
     )
@@ -46,7 +46,7 @@ async def test_set_timer_replaces_existing_timer(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify",
         ),
         "off",
     )
@@ -74,7 +74,7 @@ async def test_set_timer_none_cancels_timer(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify",
         ),
         "off",
     )
@@ -97,7 +97,7 @@ async def test_async_unload_cleans_up_timer(
     controller = DummyController(
         hass,
         MockConfigEntry(
-            domain="smart_controller",
+            domain="smartify",
         ),
         "off",
     )